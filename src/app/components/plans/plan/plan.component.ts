941dfaa8015141ff13612e3871be64ab48e26cb5
Raul Pineda <85385878+pineda24@users.noreply.github.com>
2023-05-14 23:42:26 +0200

    plan finish

--- src/app/components/plans/plan/plan.component.ts
+++ src/app/components/plans/plan/plan.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { CareerModel } from 'src/app/models/career.model';
 import { PlanModel } from 'src/app/models/plan.model';
+import { SujectModel } from 'src/app/models/subject.model';
 import { DataService } from 'src/app/services/data.service';
 
 @Component({
@@ -9,8 +11,11 @@ import { DataService } from 'src/app/services/data.service';
   styleUrls: ['./plan.component.css']
 })
 export class PlanComponent {
-  action: any = 'edit';
+  idPlan: any;
+  action: any;
   plan: PlanModel = new PlanModel();
+  arrayCarrers: Array<CareerModel> = [];
+  arraySubjects: Array<SujectModel> = [];
 
   constructor(
     private data: DataService,
@@ -18,7 +23,78 @@ export class PlanComponent {
     private router: Router
   ) { }
 
-  saveCollection() {
+  async ngOnInit() {
+    this.idPlan = this.route.snapshot.paramMap.get('id');
+    this.action = this.idPlan ? 'edit' : 'create';
+    this.getSubjects();
+    this.getCareers();
+    if (this.action == 'edit') {
+      this.data.findById('/prestamos', `${this.idPlan}/`).subscribe(
+        (res: any) => {
+          this.plan = res.prestamos[0];
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    }
+  }
 
+  saveCollection() {
+    if (this.action == 'edit') {
+      this.updateCollection();
+    } else {
+      this.createCollection();
+    }
+  }
+
+  async createCollection() {
+    this.data
+      .insertOne('/prestamos', JSON.stringify(this.plan))
+      .subscribe(
+        (res: any) => {
+          console.log(res);
+          this.router.navigate(['..'], { relativeTo: this.route });
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
+
+  async updateCollection() {
+    this.data
+      .updateOnee('/prestamos', `${this.idPlan}/`, JSON.stringify(this.plan))
+      .subscribe(
+        (res: any) => {
+          console.log(res);
+          this.router.navigate(['..'], { relativeTo: this.route });
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
+
+  getSubjects() {
+    this.data.find('/subjects').subscribe(
+      (res: any) => {
+        this.arraySubjects = res.prestamos;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  getCareers() {
+    this.data.find('/careers').subscribe(
+      (res: any) => {
+        this.arrayCarrers = res.prestamos;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 }
