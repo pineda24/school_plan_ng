18ee60e139801c3b3b8f72f40d878798ad10b3ba
Raul Pineda <85385878+pineda24@users.noreply.github.com>
2023-05-14 23:52:44 +0200

    suject table load subjects

--- src/app/components/subjects/subjects.component.ts
+++ src/app/components/subjects/subjects.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SujectModel } from 'src/app/models/subject.model';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-subjects',
@@ -6,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./subjects.component.css']
 })
 export class SubjectsComponent {
+  subjects: Array<SujectModel> = [];
 
+  constructor(
+    private data: DataService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.getSubjects();
+  }
+
+  async getSubjects() {
+    await this.data.find('/subjects').subscribe(
+      (res) => {
+        console.log(res);
+        this.subjects = res.prestamos;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  async deleteSubject(id: string) {
+    await this.data.deleteOne('/prestamos', `${id}/`).subscribe(
+      (res) => {
+        console.log(res);
+        this.getSubjects();
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
