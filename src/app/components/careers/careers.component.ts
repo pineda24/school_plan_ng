f700335a50dae572b590bab2450b5ab197bf221d
Raul Pineda <85385878+pineda24@users.noreply.github.com>
2023-05-14 23:56:05 +0200

    load careers

--- src/app/components/careers/careers.component.ts
+++ src/app/components/careers/careers.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CareerModel } from 'src/app/models/career.model';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-careers',
@@ -6,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./careers.component.css']
 })
 export class CareersComponent {
+  careers: Array<CareerModel> = [];
 
+  constructor(
+    private data: DataService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.getCareers();
+  }
+
+  async getCareers() {
+    await this.data.find('/subjects').subscribe(
+      (res) => {
+        console.log(res);
+        this.careers = res.prestamos;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  async deleteCareer(id: string) {
+    await this.data.deleteOne('/prestamos', `${id}/`).subscribe(
+      (res) => {
+        console.log(res);
+        this.getCareers();
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
